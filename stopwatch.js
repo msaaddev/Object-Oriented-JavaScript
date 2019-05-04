1bd2fffd247bedc2399e7eb66e88e75e44ac6bd2
mr-saad <mohammadsaad1612@gmail.com>
2019-05-04 13:45:11 +0500

    📦 NEW: Stopwatch methods

--- stopwatch.js
+++ stopwatch.js
@@ -1,17 +1,44 @@
 
 function Stopwatch() {
 
-    let duration  = 0;
-    this.start = () => {
+    let duration = 0;
+    let checkForStart, startTime, endTime;
 
+    this.start = () => {
+        if(checkForStart)
+            throw new Error('Stopwatch has already started');
+
+        checkForStart = true;
+
+        startTime = new Date();
     };
 
     this.stop = () => {
+        if(!checkForStart)
+            throw new Error('Stopwatch has not been started yet.');
 
+        checkForStart = false;
+
+        endTime = new Date();
+
+        const seconds = (endTime.getTime() - startTime.getTime())/1000;
+
+        duration += seconds;
     };
 
     this.reset = function () {
-        
+        startTime = null;
+        endTime = null;
+        checkForStart = false;
+        duration = 0;
     }
 
+    Object.defineProperty(this, 'duration', {
+        get: () => {return duration},
+        set: (value) => { return duration = value;}
+    })
 }
+
+const sw = new Stopwatch();
+
+console.log(sw.duration);
