d45aa82c5dcf718cb901a58dc7d7881f0592b374
mr-saad <mohammadsaad1612@gmail.com>
2019-05-04 13:21:19 +0500

    📦 NEW: structure

--- stopwatch.js
+++ stopwatch.js
@@ -1,4 +1,17 @@
 
 function Stopwatch() {
-    
+
+    let duration  = 0;
+    this.start = () => {
+
+    };
+
+    this.stop = () => {
+
+    };
+
+    this.reset = function () {
+        
+    }
+
 }
